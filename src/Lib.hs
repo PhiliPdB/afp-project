12e90f5aec7c5d8325edcf65975485dd7fbd211d
DanielvanVliet <danielvvliet@hotmail.com>
2021-03-01 12:36:19 +0100

    initial draft of datastructures

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import           Data.Vector
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type Number = Float -- will need to print rounded up nicely
+
+data Data = VNumber Vector Number | VString Vector String | VBool Vector Bool
+
+
+-- infer type of column with formula?
+data Formula = F
+
+
+data Column = ColumnD Data | ColumnF Formula
+
+newtype Spreadsheet = Spreadsheet [Column]
+
