dcff8a99056d428e2a6649dfce0f6e733952353a
PhiliPdB <phlpdbrn@gmail.com>
2021-03-06 12:10:52 +0100

    Write additional documentation and methods to print the SpreadSheet to a CSV file

--- src/Data/Column.hs
+++ src/Data/Column.hs
@@ -6,17 +6,19 @@ import Data.Type (Type(..))
 
 -- | Column datatype containing either data or a formula
 data Column a where
+    -- | Column containing data
     CData :: [a]       -> Column a
+    -- | Column containing a formula
     CForm :: Formula a -> Column a
 
+-- | Supported SpreadSheet columns
+data SpreadSheetCol = CInt    (Column Int)
+                    | CBool   (Column Bool)
+                    | CString (Column String)
 
-data SpreadSheetCols = CInt    (Column Int)
-                     | CBool   (Column Bool)
-                     | CString (Column String)
 
-
--- | Get the @Column@ out of @SpreadSheetCol@ based on the requested type
-getCol :: SpreadSheetCols -> Type a -> Column a
+-- | Get the `Column` out of `SpreadSheetCol` based on the requested type
+getCol :: SpreadSheetCol -> Type a -> Column a
 getCol (CInt c)    TInt    = c
 getCol (CBool c)   TBool   = c
 getCol (CString c) TString = c
---
+++ src/ShowCSV.hs
@@ -0,0 +1,45 @@
+module ShowCSV where
+
+import Data.SpreadSheet (SpreadSheet(..), evalSpreadSheet)
+import Data.Column (SpreadSheetCol(..), Column(CData))
+import Data.List (intercalate)
+
+
+-- | Write a `SpreadSheet` to a CSV file
+writeCSVFile :: FilePath -> SpreadSheet -> IO ()
+writeCSVFile file s = writeFile file (toCSVString s)
+
+-- | Convert a spreadsheet into an CSV string
+toCSVString :: SpreadSheet -> String
+toCSVString s@(SpreadSheet n _) = unlines $ intercalate "," headers : toCSVString' 0 columns
+    where -- | Evaluated spreadsheet
+          -- Thus contains no CFormula columns anymore.
+          evaluatedSpreadSheet = evalSpreadSheet s
+          -- | Headers of the evaluated spreadsheet
+          headers = map fst evaluatedSpreadSheet
+          -- | Columns (with only the data) of the evaluated spreadsheet
+          columns = map snd evaluatedSpreadSheet
+
+          -- | Convert the columns into an array of CSV-row-lines
+          -- This is done by keeping track of the current row (with a variable `i`)
+          -- and appending rows till `i` reaches the size of the spreadsheet.
+          --
+          -- TODO: With the current implementation for columns, this takes $O(n^2)$ time,
+          --       while $O(n)$ should be possible if indexing is $O(1)$.
+          toCSVString' :: Int -> [SpreadSheetCol] -> [String]
+          toCSVString' i cs | i < n     = intercalate "," (map (showColItem i) cs) : toCSVString' (i + 1) cs
+                            | otherwise = []
+
+          -- | Convert the `i`th item in a column to a string.
+          -- NOTE: This is done in a very unsafe way where we assume that
+          --       the column has been evaluated, thus doesn't contain
+          --       any formula's anymore.
+          showColItem :: Int            -- ^ Index of the row to 'print'
+                      -> SpreadSheetCol -- ^ Column to 'print'
+                      -> String         -- ^ Resulting string value
+          showColItem i (CInt    (CData a)) = show (a !! i)
+          showColItem i (CBool   (CData a)) = show (a !! i)
+          showColItem i (CString (CData a)) = show (a !! i)
+          showColItem _ _                   = error "Non-Evaluated spreadsheet column"
+
+
