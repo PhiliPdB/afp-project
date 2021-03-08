0a66e227ae62a929cd36a336dee2a93c69fc2c82
DanielvanVliet <danielvvliet@hotmail.com>
2021-03-08 17:24:22 +0100

    arbitrary instance for SpreadSheet

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,24 @@
+import Data.SpreadSheet
+import Data.Column
+import Test.Tasty.QuickCheck 
+-- https://hackage.haskell.org/package/QuickCheck-2.14.2/docs/Test-QuickCheck-Gen.html#t:Gen
+
+
+column :: Int -> Gen SpreadSheetCol
+column nRows = oneof [
+        CInt    . CData <$> vectorOf nRows (arbitrary :: Gen Int),
+        CBool   . CData <$> vectorOf nRows (arbitrary :: Gen Bool),
+        CString . CData <$> vectorOf nRows (arbitrary :: Gen String)
+    ]
+
+instance Arbitrary SpreadSheet where
+    arbitrary = do
+        nRows <- arbitrary :: (Gen Int)
+        nCols <- arbitrary :: (Gen Int) 
+        columnNames <- vectorOf nCols (arbitrary :: (Gen String))
+        columnData  <- vectorOf nCols (column nRows)
+        return $ SpreadSheet nRows (zip columnNames columnData)
+        
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
