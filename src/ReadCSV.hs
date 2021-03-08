83d5f94c0f54829239f0628f30a5f20b0529b91b
J.K.Mlynek <6674410@students.uu.nl>
2021-03-08 13:51:28 +0100

    Finished mainly the import function in ReadCSV.hs Tested for now with a small csv file with ints bools and strings but not with csv that has different column lengths.
    Type cheks without errors and runs, although it is hard to check
    if all data in the final spreadsheet match the csv file.

--- src/ReadCSV.hs
+++ src/ReadCSV.hs
@@ -2,24 +2,38 @@ module ReadCSV where
 
 import Data.SpreadSheet
 import Data.Column
-import Data.List.Split
+import Data.List.Split  -- for split and oneOf
+import Data.List        -- for transpose
+import Text.Read        -- for readMaybe
+import Data.Maybe       -- for fromJust
 
+-- | Takes a rawColumn in string form, checks what type they should be and
+--   replies with a SpreadSheetCols containing the correct type and the list
+--   with correct type.
+inferDataType :: [String] -> SpreadSheetCols
+inferDataType tColumn | intCol    /= Nothing = CInt    $ CData $ fromJust intCol 
+                      | boolCol   /= Nothing = CBool   $ CData $ fromJust boolCol
+                      | otherwise            = CString $ CData            tColumn
+        where
+            intCol  = sequenceA (map readMaybe tColumn :: [Maybe Int])
+            boolCol = sequenceA (map readMaybe tColumn :: [Maybe Bool])
+
+-- | Creates the spreadsheet based on the size of the rawData with default column 
+--   names and data in them based on the function inferDataType.
+createSpSh :: [[String]] -> SpreadSheet
+createSpSh rows = SpreadSheet sheetSize finalData
+        where
+            sheetSize = length rows
+            names     = ["col" ++ show x | x <- [1..sheetSize]]
+            transRows = transpose rows
+            finalData = [(colName, colData) | colName <- names, colData <- map inferDataType transRows]
 
 
 -- | Takes the path to the file, the separator that is should look for, and replies with the
 --   spreadsheet wrapen in the IO monad. 
-importCSV :: FilePath -> Char -> IO String -- SpreadSheet
+importCSV :: FilePath -> Char -> IO SpreadSheet
 importCSV fPath sep = do
                         contents <- readFile fPath
-                        let rawData   = split (oneOf [sep]) <$> lines contents
-
-                            -- the data needs to be type derived and put as a Spreadsheet
-                            -- data type in one go, as the main problem is to match
-                            -- our type system (which can be tricky to use with lists)
-                            -- with deriving and loading the types into the spreadsheet columns
-
-                            -- Figuring out the type can be done with readMaybe
-
-                        -- VVV just that it works for now and spoits out the IO String
-                        return $ head headRow
+                        let rawData = split (oneOf [sep]) <$> lines contents
+                        return $ createSpSh rawData
 
