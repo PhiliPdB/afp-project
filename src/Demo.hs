4efd6b3e3afb9070d5a4409e78d7515bae212468
DanielvanVliet <danielvvliet@hotmail.com>
2021-04-06 14:25:31 +0200

    fixed missing rows in d1

--- src/Demo.hs
+++ src/Demo.hs
@@ -18,7 +18,7 @@ d1 :: IO ()
 d1 = do
     s <- importCSV "demo/products.csv" ',' True
     let (Right s1) = s
-    let s2 = tryAddColumn s1 1 ("Origin", CString (CData ["Africa", "India", "Brazil"]))
+    let s2 = tryAddColumn s1 1 ("Origin", CString (CData ["Africa", "India", "Brazil", "India", "China"]))
     let s3 = tryAddRow s2 [FDate (HG.Date 2021 HG.April 8), FString "Netherlands", FString "Strawberries", FInt 5, FFloat 1.2, FInt 4]
     let env = spreadSheetEnv $ M.fromList [("s", s3)]
     writeCSVFile "demo/export1.csv" env s3
